e9f636567e7ede2780240c6bef4bbb7296857366
Clement <clement@voodoo.io>
2019-03-05 17:41:24 +0100

    + Add MapBoxUtilsExtended utils class for Mapbox ~ Modify User localization script Test

---
+++ Geolocalization/Assets/Voodoo/Scripts/UserLocalization.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Voodoo.Mapbox.Utils;
+using UnityEngine;
+
+[RequireComponent(typeof(AbstractMap))]
+public class UserLocalization : MonoBehaviour
+{
+    AbstractMap m_AbstractMap;
+    
+    void Awake()
+    {
+        m_AbstractMap = GetComponent<AbstractMap>();
+    }
+
+    [ContextMenu("Center Map on User")] 
+    public void SetMapToPlayerLocation()
+    {
+        double x = 48.866667;
+        double y = 2.333333;
+        
+        Vector2d coordinates = new Vector2d(x,y);
+        
+        Debug.Log("Center Map on user : " + coordinates);
+        m_AbstractMap.UpdateMap(coordinates);
+    }
+}
---
+++ Geolocalization/Assets/Voodoo/Scripts/Utils/MapBoxUtilsExtended.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mapbox.Utils;
+using UnityEngine;
+
+namespace Voodoo.Mapbox.Utils
+{
+	public static class MapBoxUtilsExtended
+	{
+		public static Vector2d ToVector2d(this Vector2 _Vector2)
+		{
+			double x = _Vector2.x;
+			double y = _Vector2.y;
+			return new Vector2d(x,y);
+		}
+
+		public static Vector2 ToVector2(this Vector2d _Vector2D)
+		{
+			float x = (float)_Vector2D.x;
+			float y = (float)_Vector2D.y;
+			return new Vector2(x,y);
+		}
+	}
+}
